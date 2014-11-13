829a8b17505a0f3b19f9d6c4f28dc51097c91e71
LakhanM <lakhan.melugiri@gmail.com>
2014-11-12 22:12:18 -0500

    Major changes
    Implemented Home, Resume, and Contact page

--- config.rb
+++ config.rb
@@ -5,11 +5,11 @@ add_import_path "bower_components/foundation/scss"
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "scss"
-images_dir = "images"
+images_dir = "Content/Images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
